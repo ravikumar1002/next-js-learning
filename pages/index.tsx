32ba3b95b7bf15f15d90a6be23e3795b2288cd96
Ravi kumar <1002kumarravi@gmail.com>
2023-04-28 11:44:45 +0530

    change word in bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home = (props: IHomeProps) => {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Ravi kumar</p>
+        <p>Hi</p>
         <p>
           This is a sample website - you’ll be building a site like this on{' '}
         </p>
